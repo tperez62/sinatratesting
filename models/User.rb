5a8d41cb52e623e34395a1d773c36cde80cddcd0
tperez62 <perezt1994@gmail.com>
2015-10-09 10:08:28 -0400

    Removed User Password

--- models/User.rb
+++ models/User.rb
@@ -4,5 +4,4 @@ class User
 	property :id		, Serial
 	property :email		, String	, required: true	, unique: true
 	property :username	, String	, required: true	, unique: true
-	property :password	, String	, required: true
 end
